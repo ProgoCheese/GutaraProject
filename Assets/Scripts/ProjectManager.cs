74aa207b6ff5f5276d48d0b5099830278a4f4520
Artemii100500 <Artemii100500@gmail.com>
2024-02-20 16:34:12 +0700

    Is work

--- Assets/Scripts/ProjectManager.cs
+++ Assets/Scripts/ProjectManager.cs
@@ -2,6 +2,53 @@ using UnityEngine;
 
 public class ProjectManager : MonoBehaviour
 {
+    /// <summary>
+    /// 1.AllList
+    /// 2.AllSing
+    /// 3.BazaList
+    /// 4.CoiList
+    /// 5.DifferentList 
+    /// </summary>
+
+    public string lastMenu;
+
+    public GameObject[] arrMenu;
+
+    public void SetLastMenu(string id)
+    {
+        lastMenu = id;
+    }
+
+    public void GetLastMenu()
+    {
+        switch (lastMenu) 
+        {
+            case "AllList":
+                arrMenu[0].gameObject.SetActive(true);
+                //Debug.Log("0");
+                break;
+            case "AllSing":
+                arrMenu[1].gameObject.SetActive(true);
+                //Debug.Log("1");
+                break;
+            case "BazaList":
+                arrMenu[2].gameObject.SetActive(true);
+                //Debug.Log("2");
+                break;
+            case "CoiList":
+                arrMenu[3].gameObject.SetActive(true);
+                //Debug.Log("3");
+                break;
+            case "DifferentList":
+                arrMenu[4].gameObject.SetActive(true);
+                //Debug.Log("3");
+                break;
+            default:
+                //Debug.Log("4?");
+                break;
+        }
+    }
+
     public void QuitGame()
     {
         Application.Quit();
